fa0471a7b31a8813a8ad62f25f03ee040e4a66b4
stellanechi <stellanechi65@gmail.com>
2025-11-04 03:12:20 +0100

    created signup pages

--- src/auth/AuthLayout.vue
+++ src/auth/AuthLayout.vue
@@ -1,13 +1,12 @@
 <script>
-import SignUpone from "@/pages/signup/SignUpone.vue";
+import SignUpStepOne from "@/pages/signup/SignUpStepOne.vue";
 
 export default {
   name: "AuthLayout",
-  components: { SignUpone },
+  components: { SignUpStepOne },
 };
 </script>
 
 <template>
-  <!-- <div class="bg-red-800 h-80">App Entry point</div> -->
-  <SignUpone />
+  <SignUpStepOne />
 </template>
