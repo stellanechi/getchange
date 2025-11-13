b3a9aad976d2ed2ab7bb3181e77e3a03092b3afe
stellanechi <stellanechi65@gmail.com>
2025-11-13 09:18:36 +0100

    updated explore

--- src/pages/dashboard/Explore.vue
+++ src/pages/dashboard/Explore.vue
@@ -3,6 +3,7 @@ import ExploreTable from "@/components/explore/ExploreTable.vue";
 import Modal from "@/components/explore/Modal.vue";
 import QuickView from "@/components/explore/QuickView.vue";
 import Button from "@/components/Button.vue";
+import Calculator from "@/components/Calculator.vue";
 
 export default {
   name: "AuthLayout",
@@ -11,6 +12,7 @@ export default {
     ExploreTable,
     Modal,
     Button,
+    Calculator,
   },
   data() {
     return {
@@ -44,38 +46,43 @@ export default {
 </script>
 
 <template>
-  <div>
-    <div class="mb-6">
-      <QuickView title="Store Wallet" :amount="2500" @onAction="handleTopUp">
-        <Button
-          buttonText="Top up wallet"
-          buttonBgColor="#22c55e"
-          buttonTextColor="#ffffff"
-          @onAction="handleClick"
-        />
-      </QuickView>
-    </div>
+  <div class="flex justify-between align-center">
+    <div>
+      <div class="mb-6 max-width-2xl">
+        <QuickView title="Store Wallet" :amount="2500" @onAction="handleTopUp">
+          <Button
+            buttonText="Top up wallet"
+            buttonBgColor="#22c55e"
+            buttonTextColor="#ffffff"
+            @onAction="handleClick"
+          />
+        </QuickView>
+      </div>
 
-    <!-- Total Disbursed -->
-    <div class="mb-6">
-      <QuickView
-        title="Total Disbursed"
-        :amount="12000"
-        @onAction="viewHistory"
-      >
-        <Button
-          buttonText="View History"
-          buttonBgColor="#ffffff"
-          buttonTextColor="#0F172A"
-          :borderVisible="true"
-          @onAction="handleClick"
-      /></QuickView>
+      <!-- Total Disbursed -->
+      <div class="mb-6">
+        <QuickView
+          title="Total Disbursed"
+          :amount="12000"
+          @onAction="viewHistory"
+        >
+          <Button
+            buttonText="View History"
+            buttonBgColor="#ffffff"
+            buttonTextColor="#0F172A"
+            :borderVisible="true"
+            @onAction="handleClick"
+        /></QuickView>
+      </div>
+      <div>
+        <ExploreTable />
+      </div>
+
+      <Modal v-model="showModal" @credit-wallet="handleCreditWallet" />
     </div>
     <div>
-      <ExploreTable />
+      <Calculator />
     </div>
-
-    <Modal v-model="showModal" @credit-wallet="handleCreditWallet" />
   </div>
 </template>
 
